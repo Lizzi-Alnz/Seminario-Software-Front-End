660681f13681a1a0ce323398568e96a9b9876f40
Lizzi-Alonzo <lizzimalore@gmail.com>
2022-07-07 15:04:07 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Summary from './Views/Summary';
+
+function App() {
+  return (
+   <>
+    <header>
+    <h1>SW 202202</h1>
+    <h2>Ochenta</h2>
+    </header>
+    <main>
+      <Summary></Summary>
+    </main>
+    <footer>
+    <div>Derechos Reservados 2022 ICC Unicah</div>
+    </footer>
+   </>
+  );
+}
+
+export default App;
